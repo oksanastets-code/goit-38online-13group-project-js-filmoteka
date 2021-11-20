5e565a2f18e6c21e7ec593a1a760d64342c66c93
Roman Vozniuk <muzzlestore@gmail.com>
2021-11-20 20:30:25 +0200

    add gallery cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './sass/main.scss';
 import './js/modalTeam.js';
 import './js/spinner';
+
+
+
+// черновик запросов
+import API from './js/apiService.js';
+import imageCardTmp from './templates/movie-card.hbs';
+
+const getFilm = new API();
+
+
+
+const imagesGallery = document.querySelector('.card__container');
+
+
+getFilm.fetchImages().then(renderImageCard).then(r => console.log(r));
+
+
+function renderImageCard(film) {
+  imagesGallery.insertAdjacentHTML('beforeend', imageCardTmp(film));
+}
+// черновик запросов
