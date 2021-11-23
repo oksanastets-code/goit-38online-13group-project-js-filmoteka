deb1dbc46c86aceb99de25ea72b3719580c9cdac
Roman Vozniuk <muzzlestore@gmail.com>
2021-11-23 23:44:16 +0200

    add new api for modal

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -26,10 +26,12 @@ export default class moviesApiService {
   getMovieById(id) {
     return fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
       .then(r => r.json())
-      .then(( results ) => {      
-        return results;})
-
+      .then(({ ...results } ) => {
+         results.genres = results.genres.map(genre => genre.name);
+         return results;       
+      });
   }
+
   getMoviesByQuery() {
     return fetch(`${BASE_URL}/search/movie?query=${this.query}&api_key=${API_KEY}`)
       .then(r => r.json())
@@ -44,22 +46,6 @@ export default class moviesApiService {
          });
       });
 }
-
-
-  // getMovieById(id) {
-  //   return fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
-  //     .then(r => r.json())
-  //     .then(({ results }) => {      
-  //       return this.getGenres()
-  //         .then(r => {
-  //           return results.map(film => ({
-  //             ...film,
-  //             genre_ids: this.getGenreName(r, film.genre_ids)
-  //           })
-  //           );
-  //         });
-  //     });
-  // }
 
   getGenres() {
     return fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`)
