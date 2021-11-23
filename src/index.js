05b42dbaf39241e2029c9ebcb6d2c51f6a490674
Kostianyn <odessavshok@gmail.com>
2021-11-23 07:38:58 +0200

    add up-button
    добавляет кнопу вверх для карточек

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './sass/main.scss';
 import './js/modalTeam.js';
 import './js/spinner';
-import './js/modalFilm.js'
+import './js/modalFilm.js';
 import './js/page-Home.js';
 import './js/page-Library.js';
 import './js/localStorage.js';
-
+import './js/up-btn.js';
 
 // черновик запросов
 import API from './js/apiService.js';
@@ -13,16 +13,12 @@ import imageCardTmp from './templates/movie-card.hbs';
 
 const getFilm = new API();
 
-
-
 const imagesGallery = document.querySelector('.card__container');
-
 
 getFilm.getTrendingMovies().then(renderImageCard);
 // getFilm.getMovieById(8).then(renderImageCard);
 function renderImageCard(film) {
   imagesGallery.insertAdjacentHTML('beforeend', imageCardTmp(film));
- 
 }
 
 // черновик запросов
