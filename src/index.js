d7d0913476bc5911842cdd9ec4c5f8bd3549ea9a
muzzlegit <82829794+muzzlegit@users.noreply.github.com>
2021-11-23 23:20:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,24 +4,6 @@ import './js/spinner';
 import './js/modalFilm.js'
 import './js/page-Home.js';
 import './js/page-Library.js';
+import './js/localStorage.js';
 
 
-
-// черновик запросов
-import API from './js/apiService.js';
-import imageCardTmp from './templates/movie-card.hbs';
-
-const getFilm = new API();
-
-
-
-const imagesGallery = document.querySelector('.card__container');
-
-
-getFilm.getTrendingMovies().then(renderImageCard);
-// getFilm.getMovieById(8).then(renderImageCard);
-function renderImageCard(film) {
-  imagesGallery.insertAdjacentHTML('beforeend', imageCardTmp(film));
-}
-
-// черновик запросов
